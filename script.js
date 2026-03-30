f05af795f5b2702412027f70d3a1cc3a78a47b8a
Chavan Sakshi Pandurang <sakshichavan4151@gmail.com>
2026-03-30 18:29:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+function validateForm(e) {
+    e.preventDefault();
 
+    const btn = document.querySelector("#form button");
+    btn.classList.remove("success-btn");
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const msg = document.getElementById("msg").value.trim();
+    const error = document.getElementById("error");
+
+    error.classList.remove("success");
+
+    if (name === "") {
+        error.innerText = "Name is required";
+        return;
+    }
+
+    if (email === "") {
+        error.innerText = "Email is required";
+        return;
+    }
+
+    if (!email.includes("@")) {
+        error.innerText = "Enter valid email";
+        return;
+    }
+
+    if (msg === "") {
+        error.innerText = "Message is required";
+        return;
+    }
+
+    error.classList.add("success");
+    btn.classList.add("success-btn");
+    error.innerText = "Form submitted successfully!";
+}
+
+document.getElementById("form").addEventListener("submit", validateForm);
